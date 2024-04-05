a869d73d9a5a4e23629c5b2e67ea4edc0151a214
Alejandro <alejandrolopezfuentes789@gmail.com>
2024-04-05 20:13:32 +0200

    start

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ps_config_with_oh-my-posh
+
+## How to install
+```
+(Invoke-WebRequest "https://raw.githubusercontent.com/Alopezfu/ps_config_with_oh-my-posh/main/install.ps1").Content | Invoke-Expression
+```
--- install.ps1
+++ install.ps1
@@ -1,5 +1,16 @@
 $PS_PROFILE = "https://raw.githubusercontent.com/devblackops/Terminal-Icons/main/psakeFile.ps1"
 
+Write-Host ''                                                  ,d8888b  d8,                               d8,         d8b                   d8b                                                                 d8b      
+                                                  88P'    `8P                               `8P    d8P   ?88                   ?88                                                                 ?88      
+                                               d888888P                                         d888888P  88b                   88b                                                                 88b     
+?88,.d88b, .d888b,     d8888b d8888b   88bd88b   ?88'      88b d888b8b       ?88   d8P  d8P  88b  ?88'    888888b      d8888b   888888b       88bd8b,d88b ?88   d8P     ?88,.d88b, d8888b  .d888b,  888888b 
+`?88'  ?88 ?8b,       d8P' `Pd8P' ?88  88P' ?8b  88P       88Pd8P' ?88       d88  d8P' d8P'  88P  88P     88P `?8b    d8P' ?88  88P `?8b      88P'`?8P'?8bd88   88      `?88'  ?88d8P' ?88 ?8b,     88P `?8b
+  88b  d8P   `?8b     88b    88b  d88 d88   88P d88       d88 88b  ,88b      ?8b ,88b ,88'  d88   88b    d88   88P    88b  d88 d88   88P     d88  d88  88P?8(  d88        88b  d8P88b  d88   `?8b  d88   88P
+  888888P'`?888P'     `?888P'`?8888P'd88'   88bd88'      d88' `?88P'`88b     `?888P'888P'  d88'   `?8b  d88'   88b    `?8888P'd88'   88b    d88' d88'  88b`?88P'?8b       888888P'`?8888P'`?888P' d88'   88b
+  88P'                                                               )88                                                                                         )88      88P'                              
+ d88                                                                ,88P                                                                                        ,d8P     d88                                
+ ?8P                                                            `?8888P                                                                                      `?888P'     ?8P                                
+''
 Write-Host "Copy Powershell profile"
 (Invoke-WebRequest $PS_PROFILE).Content > $PROFILE
 
@@ -11,5 +22,3 @@ Import-Module terminal-Icons
 
 Write-Host "Install posh-git module"
 Install-Module posh-git -Force
-
-
