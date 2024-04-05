bc2cec5a31274f184632d3474e05215900377f80
Alejandro <alejandrolopezfuentes789@gmail.com>
2024-04-05 20:18:19 +0200

    start

--- install.ps1
+++ install.ps1
@@ -1,16 +1,14 @@
 $PS_PROFILE = "https://raw.githubusercontent.com/Alopezfu/ps_config_with_oh-my-posh/main/profile.ps1"
 
-Write-Host ''                                                  ,d8888b  d8,                               d8,         d8b                   d8b                                                                 d8b      
-                                                  88P'    `8P                               `8P    d8P   ?88                   ?88                                                                 ?88      
-                                               d888888P                                         d888888P  88b                   88b                                                                 88b     
-?88,.d88b, .d888b,     d8888b d8888b   88bd88b   ?88'      88b d888b8b       ?88   d8P  d8P  88b  ?88'    888888b      d8888b   888888b       88bd8b,d88b ?88   d8P     ?88,.d88b, d8888b  .d888b,  888888b 
-`?88'  ?88 ?8b,       d8P' `Pd8P' ?88  88P' ?8b  88P       88Pd8P' ?88       d88  d8P' d8P'  88P  88P     88P `?8b    d8P' ?88  88P `?8b      88P'`?8P'?8bd88   88      `?88'  ?88d8P' ?88 ?8b,     88P `?8b
-  88b  d8P   `?8b     88b    88b  d88 d88   88P d88       d88 88b  ,88b      ?8b ,88b ,88'  d88   88b    d88   88P    88b  d88 d88   88P     d88  d88  88P?8(  d88        88b  d8P88b  d88   `?8b  d88   88P
-  888888P'`?888P'     `?888P'`?8888P'd88'   88bd88'      d88' `?88P'`88b     `?888P'888P'  d88'   `?8b  d88'   88b    `?8888P'd88'   88b    d88' d88'  88b`?88P'?8b       888888P'`?8888P'`?888P' d88'   88b
-  88P'                                                               )88                                                                                         )88      88P'                              
- d88                                                                ,88P                                                                                        ,d8P     d88                                
- ?8P                                                            `?8888P                                                                                      `?888P'     ?8P                                
-''
+Write-Host '
+  ____            _   _       ____        _           _____          _             
+ |  _ \ ___  _ __| |_(_) __ _| __ )  ___ | |_ ___    |_   _|__   ___| |_ ___  _ __ 
+ | |_) / _ \| '__| __| |/ _` |  _ \ / _ \| __/ _ \_____| |/ _ \ / __| __/ _ \| '__|
+ |  __/ (_) | |  | |_| | (_| | |_) | (_) | ||  __/_____| | (_) | (__| || (_) | |   
+ |_|   \___/|_|   \__|_|\__, |____/ \___/ \__\___|     |_|\___/ \___|\__\___/|_|   
+                         |___/                                                      
+' -ForegroundColor Yellow
+
 Write-Host "Copy Powershell profile"
 (Invoke-WebRequest $PS_PROFILE).Content > $PROFILE
 
