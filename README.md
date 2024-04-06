aec48ec7037abcdaa89f204f5dee929c2d101e07
Alejandro <alejandrolopezfuentes789@gmail.com>
2024-04-06 14:31:02 +0200

    start

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## How to install
 ```
-(Invoke-WebRequest "https://raw.githubusercontent.com/Alopezfu/ps_config_with_oh-my-posh/main/install.ps1").Content | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope Process -Force ;  ./install.ps1
 ```
--- install.ps1
+++ install.ps1
@@ -1,4 +1,3 @@
-$PS_PROFILE = "https://raw.githubusercontent.com/Alopezfu/ps_config_with_oh-my-posh/main/profile.ps1"
 $BANNER = @"
  
   ██████╗ ███████╗     ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗ 
@@ -9,22 +8,52 @@ $BANNER = @"
   ╚═╝     ╚══════╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ 
   
           by github.com/Alopezfu/
-           github.com/Alopezfu/ps_config_with_oh-my-posh
  
 "@
 
+$FONT="JetBrainsMono Nerd Font"
+
+function Write($message){
+
+  Write-Host 
+}
+
 Clear
 Write-Host -ForegroundColor Yellow $BANNER
-Sleep 4
+Sleep 3
 
-Write-Host "Copy Powershell profile"
-(Invoke-WebRequest $PS_PROFILE).Content > $PROFILE
+Write-Host -NoNewline -ForegroundColor Blue "[!] Copy Powershell profile"
+Get-Content .\profile.ps1 > $PROFILE
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
 
-Write-Host "Install oh-my-posh"
+Write-Host -ForegroundColor Blue "[!] Install oh-my-posh"
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
 
-Write-Host "Install terminal-Icons module"
+Write-Host -NoNewline -ForegroundColor Blue "[!] Install JetBrainsMono Nerd font"
+oh-my-posh.exe font install JetBrainsMono --user
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
+
+$jsonContent = Get-Content -Path "$(Get-ChildItem $HOME\AppData\Local\Packages\ | Select-String WindowsTerminal)\LocalState\settings.json"
+$newJsonContent = $jsonContent.Replace('"defaults": {},', '"defaults": { "font": {"face": "' + $FONT + '"} },')
+$newJsonContent | Set-Content -Path "$(Get-ChildItem $HOME\AppData\Local\Packages\ | Select-String WindowsTerminal)\LocalState\settings.json"
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
+
+Write-Host -NoNewline -ForegroundColor Blue "[!] Install git with winget"
+winget install --id Git.Git -e --source winget
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
+
+Write-Host -NoNewline -ForegroundColor Blue "[!] Install terminal-Icons module"
 Import-Module terminal-Icons
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
 
-Write-Host "Install posh-git module"
+Write-Host -NoNewline -ForegroundColor Blue "[!] Install posh-git module"
 Install-Module posh-git -Force
+($?) ? (Write-Host -ForegroundColor Green " [OK]") : (Write-Host -ForegroundColor Red " [KO]")
+Sleep 2
